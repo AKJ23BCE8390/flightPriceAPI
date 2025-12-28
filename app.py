9738e3b21ed00a4885241755a9ab5afb41762254
Ankit Kumar <ankitkumarjhajharia001@gmail.com>
2025-12-29 04:59:25 +0530

    added cors in flast code

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 import pandas as pd
 import os
 import gdown
 
 app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 MODEL_PATH = "flight_price_pipeline.pkl"
 GDRIVE_FILE_ID = "1h0FOou99ilrXdbiqFZEBk5EZhTKJRi2q"
